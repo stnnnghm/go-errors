8cda3b4cdc2c7056a784204ab65c8c44d4528573
stvnn.sqrd <67979808+stnnnghm@users.noreply.github.com>
2021-04-21 22:47:33 -0700

    add context fields to logrus error logging

--- main.go
+++ main.go
@@ -2,51 +2,27 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
-type HttpError struct {
-	status     int
-	httpMethod string
-}
+func errorFunc() error {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	ctxFields := logrus.Fields{
+		"app":         "go-errors",
+		"environment": "dev",
+	}
 
-// HttpError implements Error method
-func (httpError *HttpError) Error() string {
-	return fmt.Sprintf("Request %v failed. Server returned %v status", httpError.httpMethod, httpError.status)
-}
-
-func GetServerResponse() (string, error) {
-	// return mocked error
-	return "", &HttpError{401, "GET"}
-}
-
-func throwAnotherError() error {
-	return errors.New("threw another error")
-}
-
-func throwError() error {
-	err := throwAnotherError()
+	err := errors.New("Error!")
 	if err != nil {
-		return errors.Wrap(err, "Error occurred while processing function throwError")
+		logrus.WithFields(ctxFields).WithError(err).Error("ErrMSg")
+		return err
 	}
 	return nil
 }
 
 func main() {
-	err := throwError()
-	if err != nil {
-		logrus.Error("Error occurred:", fmt.Sprintf("%+v", err))
-	} else {
-		// Handle server response
-		response, err := GetServerResponse()
-		if err != nil {
-			fmt.Println(err)
-			// errval returns a pointer to the instance of err as HttpError
-			errval := err.(*HttpError)
-			fmt.Printf("erroring method %v", errval.httpMethod)
-		} else {
-			fmt.Printf("server response %v", response)
-		}
-	}
+	err := errorFunc()
+	logrus.Error("Error thrown: ", fmt.Sprintf("%v", err))
 }
